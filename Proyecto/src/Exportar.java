8c1aef775c74002a885c356ea29a6c5cd23f603d
mgarclor <mgarclor@everis.nttdata.com>
2021-04-23 13:15:43 +0200

    añadir datos al fichero CSV

---
+++ Proyecto/src/Exportar.java
@@ -0,0 +1,46 @@
+
+import com.csvreader.CsvWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Iterator;
+
+
+
+
+public class Exportar {
+
+
+    public static void ExportarCSV(List<String[]> procesado) {
+
+        String archivoCSV = "C:\\archivo.csv";
+
+        try {
+
+            CsvWriter writer = new CsvWriter(archivoCSV);
+
+            writer.write("Título");
+            writer.write("Creador");
+            writer.write("Fecha de publicación");
+            writer.write("Categoría");
+
+
+            writer.endRecord();
+
+
+            Iterator i = procesado.iterator();
+            while(i.hasNext())
+            {
+                writer.write(i.next().toString());
+                writer.endRecord();
+            }
+
+            writer.close();
+
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
