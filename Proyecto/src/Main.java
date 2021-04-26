dd84fdeafc00768a63705e0eb4287b77ab1ec06a
Pedro <pedrosanmi23@gmail.com>
2021-04-26 11:00:20 +0200

    Modificacion metodo traerdatos

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Traer_Datos td = new Traer_Datos();
-//        td.traerDatos();
+        Traer_Datos td = new Traer_Datos();
+        td.traerDatos("","");
         Procesador p = new Procesador();
         List<String[]> datos = p.procesarHTML();
     }
